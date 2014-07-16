26a1c5be1e09e49c380add68f3fabc4119a8bebb
frankrs <frankrs3d@gmail.com>
2014-07-16 17:18:13 -0400

    .09
    Scripts

---
+++ Assets/scripts/Movement.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+var moveInput : MoveInput;
+class MoveInput{
+var fb : float;
+var lr : float;
+}
+
+
+
+
+var gravity : float = 9.7;
+
+private var cc : CharacterController;
+private var anim : Animator;
+
+function Awake () {
+// get character controller
+cc = GetComponent(CharacterController);
+// get animator 
+anim = GetComponent(Animator);
+}
+
+function Update () {
+// set the animator controlers variables according to the input
+anim.SetFloat("FB",moveInput.fb);
+anim.SetFloat("LR",moveInput.lr);
+}
+
+
+
+function LateUpdate(){
+// pull character down with gravity
+cc.Move(Vector3(0,-gravity*Time.deltaTime,0));
+}
+
+
+@ script RequireComponent(CharacterController)
+@ script RequireComponent(Animator)
---
+++ Assets/scripts/XBoxInput.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var xBInput : MoveInput;
+
+private var movement : Movement;
+
+function Start () {
+// get the movement input
+movement = GetComponent(Movement);
+
+}
+
+function Update () {
+// set the variables according to the input manager
+xBInput.fb = Input.GetAxis("Vertical");
+xBInput.lr = Input.GetAxis("Horizontal");
+// set the characters input controls to the ones captured here
+movement.moveInput = xBInput;
+}
+
+
+
+@ script RequireComponent(Movement)
