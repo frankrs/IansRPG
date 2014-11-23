8883c87f7d467157dba49df56e872d187a3de834
frankrs <frankrs3d@gmail.com>
2014-11-22 22:29:34 -0500

    2.154
    imported some animation

--- Assets/scripts/StagePlayer.cs
+++ Assets/scripts/StagePlayer.cs
@@ -4,14 +4,31 @@ using System.Collections;
 public class StagePlayer : MonoBehaviour {
 
 	public MonoBehaviour[] moveControls;
+	public NPCMovement move;
 	
 		IEnumerator BattleStations (Transform tran){
+
+		//set the animator
+		move.anim.applyRootMotion = false;
+		move.anim.SetBool("BattleWalk",true);
+
+		// turn on the navmaesh
+		move.nav.enabled = true;
+		move.nav.SetDestination(tran.position);
+
+		// disable normal controls
 		foreach(MonoBehaviour m in moveControls){
 			m.enabled = false;
 		}
-			while(this){
+		// move until you are in place
+		while(Vector3.Distance(transform.position, move.nav.destination) >= .25f){
 				yield return new WaitForEndOfFrame();
 			}
+		//turn to correct direction
+		transform.rotation = tran.rotation;
+
 		}
 
 }
+
+
